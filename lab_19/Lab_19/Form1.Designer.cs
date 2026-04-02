83a475ebdc4856a21b77d0b77165dd708004f179
Drakyshen <karinatisenko138@gmail.com>
2026-04-02 21:53:54 +0300

    19

---
+++ lab_19/Lab_19/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Lab_19
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            
+            this.txtInput.Location = new System.Drawing.Point(30, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(500, 22);
+            this.txtInput.TabIndex = 0;
+            this.btnExecute.Location = new System.Drawing.Point(30, 70);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(120, 40);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Виконати";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            
+            this.txtOutput.Location = new System.Drawing.Point(30, 130);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(500, 150);
+            this.txtOutput.TabIndex = 2;
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 320);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Лабораторна 19 - Варіант 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
---
+++ lab_19/Lab_19/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Lab_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+         
+            string input = txtInput.Text;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("Будь ласка, введіть текст!");
+                return;
+            }
+
+            
+            string result = Regex.Replace(input, @"\s+", " ");
+
+           
+            txtOutput.Text = result.Trim();
+        }
+    }
+}
