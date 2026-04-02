6413b26604831811071bd3b8531e8d3f17e960e3
Drakyshen <karinatisenko138@gmail.com>
2026-04-02 21:12:28 +0300

    18

---
+++ lab_18/lab18/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace lab18
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+      
+        /// <param name="disposing"
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            
+            this.txtN.Location = new System.Drawing.Point(45, 45);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(140, 22);
+            this.txtN.TabIndex = 0;
+            this.txtN.TextChanged += new System.EventHandler(this.txtN_TextChanged);
+            
+            this.btnRun.Location = new System.Drawing.Point(45, 94);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(140, 43);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Виконати";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click_1);
+             
+            this.txtOutput.Location = new System.Drawing.Point(276, 44);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(473, 354);
+            this.txtOutput.TabIndex = 2;
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ lab_18/lab18/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lab18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click_1(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            Random rnd = new Random();
+
+            if (!int.TryParse(txtN.Text, out int n) || n <= 0)
+            {
+                MessageBox.Show("Будь ласка, введіть число n!");
+                return;
+            }
+
+            double[] A = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = Math.Round(rnd.NextDouble() * 20 - 10, 2);
+            }
+
+            txtOutput.AppendText("Завдання 1:\r\n");
+            txtOutput.AppendText("Масив: " + string.Join("  ", A) + "\r\n");
+
+            double sumNeg = 0;
+            foreach (double x in A) if (x < 0) sumNeg += x;
+            txtOutput.AppendText("Сума від'ємних: " + sumNeg + "\r\n");
+
+            int minIdx = 0, maxIdx = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (A[i] < A[minIdx]) minIdx = i;
+                if (A[i] > A[maxIdx]) maxIdx = i;
+            }
+
+            int start = Math.Min(minIdx, maxIdx);
+            int end = Math.Max(minIdx, maxIdx);
+            double prod = 1;
+            bool hasBetween = false;
+            for (int i = start + 1; i < end; i++)
+            {
+                prod *= A[i];
+                hasBetween = true;
+            }
+            txtOutput.AppendText("Добуток між мін та макс: " + (hasBetween ? prod : 0) + "\r\n");
+
+            Array.Sort(A);
+            txtOutput.AppendText("Відсортовано: " + string.Join("  ", A) + "\r\n\r\n");
+
+            txtOutput.AppendText("Завдання 2 (Матриця 3x4):\r\n");
+            int[,] m = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    m[i, j] = rnd.Next(10, 100);
+                    txtOutput.AppendText(m[i, j] + "\t");
+                }
+                txtOutput.AppendText("\r\n");
+            }
+            txtOutput.AppendText("Правий верхній: " + m[0, 3] + "\r\n");
+            txtOutput.AppendText("Лівий нижній: " + m[2, 0] + "\r\n");
+        }
+
+        private void txtN_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
