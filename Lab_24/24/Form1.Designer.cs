fd069739722d4fc63ff404c566fa953620e9c9bd
Drakyshen <karinatisenko138@gmail.com>
2026-06-13 00:58:32 +0300

    24

---
+++ Lab_24/24/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace _24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(632, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(632, 398);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(223, 52);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1222, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(223, 52);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1222, 398);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(223, 52);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(277, 469);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(919, 52);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(277, 527);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(919, 52);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 250);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(570, 34);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(394, 250);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(1054, 34);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(391, 250);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1466, 591);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Lab_24/24/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace _24
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+        private CancellationTokenSource cts1;
+        private CancellationTokenSource cts2;
+        private CancellationTokenSource cts3;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Запустити 1 потік";
+            button2.Text = "Зупинити 1 потік";
+            button3.Text = "Запустити 2 потік";
+            button4.Text = "Зупинити 2 потік";
+            button5.Text = "Запустити 3 потік";
+            button6.Text = "Зупинити 3 потік";
+            button7.Text = "Запустити всі потоки";
+            button8.Text = "Зупинити всі потоки";
+        }
+
+        private void RC5_Worker(CancellationToken token)
+        {
+            const int w = 32;          
+            const int r = 12;          
+            const int b = 16;          
+            const int u = w / 8;       
+            const int t = 2 * (r + 1); 
+
+            uint P32 = 0xB7E15163u;
+            uint Q32 = 0x9E3779B9u;
+
+            Random rng = new Random();
+            int iteration = 0;
+
+            while (!token.IsCancellationRequested)
+            {
+                iteration++;
+
+                byte[] key = new byte[b];
+                rng.NextBytes(key);
+
+                uint A = (uint)rng.Next();
+                uint B_val = (uint)rng.Next();
+
+                uint[] L = new uint[b / u + 1];
+                for (int i = b - 1; i >= 0; i--)
+                    L[i / u] = (L[i / u] << 8) + key[i];
+
+                uint[] S = new uint[t];
+                S[0] = P32;
+                for (int i = 1; i < t; i++)
+                    S[i] = S[i - 1] + Q32;
+
+                uint AA = 0, BB = 0;
+                int jj = 0;
+                int n = 3 * Math.Max(t, b / u);
+                for (int k = 0; k < n; k++)
+                {
+                    S[AA] = RotL(S[AA] + AA + BB, 3);
+                    AA = S[AA];
+                    L[jj] = RotL(L[jj] + AA + BB, (int)(AA + BB));
+                    BB = L[jj];
+                    AA = (AA + 1) % t;
+                    jj = (jj + 1) % (b / u);
+                }
+
+                A = A + S[0];
+                B_val = B_val + S[1];
+                for (int i = 1; i <= r; i++)
+                {
+                    A = RotL(A ^ B_val, (int)B_val) + S[2 * i];
+                    B_val = RotL(B_val ^ A, (int)A) + S[2 * i + 1];
+                }
+
+                string result = $"[RC5 ітерація {iteration}]\r\n" +
+                                $"  Ключ (hex): {BitConverter.ToString(key)}\r\n" +
+                                $"  Зашифровано: A=0x{A:X8}  B=0x{B_val:X8}\r\n\r\n";
+
+                AppendToBox(richTextBox1, result);
+                Thread.Sleep(600);
+            }
+        }
+        private static uint RotL(uint val, int shift)
+        {
+            shift &= 31;
+            return (val << shift) | (val >> (32 - shift));
+        }
+        private void MD5_Worker(CancellationToken token)
+        {
+            Random rng = new Random();
+            int iteration = 0;
+
+            while (!token.IsCancellationRequested)
+            {
+                iteration++;
+                int len = rng.Next(8, 33);
+                byte[] buf = new byte[len];
+                rng.NextBytes(buf);
+                string input = Convert.ToBase64String(buf).Substring(0, len);
+
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+                    string hexHash = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+                    string result = $"[MD5 ітерація {iteration}]\r\n" +
+                                    $"  Вхід:  {input}\r\n" +
+                                    $"  Hash:  {hexHash}\r\n\r\n";
+
+                    AppendToBox(richTextBox2, result);
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private void Caesar_Worker(CancellationToken token)
+        {
+            string[] samples =
+            {
+                "ПРИВІТ СВІТ",
+                "HELLO WORLD",
+                "ШИФР ЦЕЗАРЯ",
+                "КРИПТОГРАФІЯ",
+                "SECRET MESSAGE",
+                "АЛГОРИТМ ШИФРУВАННЯ",
+                "БЕЗПЕКА ДАНИХ"
+            };
+
+            Random rng = new Random();
+            int iteration = 0;
+
+            while (!token.IsCancellationRequested)
+            {
+                iteration++;
+                int shift = rng.Next(1, 26);
+                string plain = samples[rng.Next(samples.Length)];
+                string cipher = CaesarEncrypt(plain, shift);
+                string decrypted = CaesarEncrypt(cipher, 26 - shift);
+
+                string result = $"[Цезар ітерація {iteration}]  зсув={shift}\r\n" +
+                                $"  Відкрито:    {plain}\r\n" +
+                                $"  Зашифровано: {cipher}\r\n" +
+                                $"  Розшифровано:{decrypted}\r\n\r\n";
+
+                AppendToBox(richTextBox3, result);
+                Thread.Sleep(700);
+            }
+        }
+
+        private static string CaesarEncrypt(string text, int shift)
+        {
+            char[] result = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (c >= 'A' && c <= 'Z')
+                    result[i] = (char)(((c - 'A' + shift) % 26) + 'A');
+                else if (c >= 'a' && c <= 'z')
+                    result[i] = (char)(((c - 'a' + shift) % 26) + 'a');
+                else if (c >= 'А' && c <= 'Я')  
+                    result[i] = (char)(((c - 'А' + shift) % 32) + 'А');
+                else if (c >= 'а' && c <= 'я')  
+                    result[i] = (char)(((c - 'а' + shift) % 32) + 'а');
+                else
+                    result[i] = c;
+            }
+            return new string(result);
+        }
+
+        private void StartThread(ref Thread t, ref CancellationTokenSource cts, Action<CancellationToken> worker)
+        {
+            if (t != null && t.IsAlive) return;
+            cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            t = new Thread(() => worker(token)) { IsBackground = true };
+            t.Start();
+        }
+ 
+        private void StopThread(ref CancellationTokenSource cts)
+        {
+            cts?.Cancel();
+        }
+
+        private void AppendToBox(RichTextBox box, string text)
+        {
+            if (box.InvokeRequired)
+                box.Invoke(new Action(() => {
+                    box.AppendText(text);
+                    box.ScrollToCaret();
+                }));
+            else
+            {
+                box.AppendText(text);
+                box.ScrollToCaret();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartThread(ref thread1, ref cts1, RC5_Worker);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopThread(ref cts1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartThread(ref thread2, ref cts2, MD5_Worker);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StopThread(ref cts2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StartThread(ref thread3, ref cts3, Caesar_Worker);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StopThread(ref cts3);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            StartThread(ref thread1, ref cts1, RC5_Worker);
+            StartThread(ref thread2, ref cts2, MD5_Worker);
+            StartThread(ref thread3, ref cts3, Caesar_Worker);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            StopThread(ref cts1);
+            StopThread(ref cts2);
+            StopThread(ref cts3);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) { }
+        private void richTextBox2_TextChanged(object sender, EventArgs e) { }
+        private void richTextBox3_TextChanged(object sender, EventArgs e) { }
+    }
+}
